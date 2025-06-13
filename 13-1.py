0ca2124779e6d2e503f2a019ba03f48a1b68bc99
황병규 <128976098+banggi0323@users.noreply.github.com>
2025-06-13 15:08:37 +0900

    13-1

---
+++ 13-1.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import ttk
+
+def buildgui():
+    label1 = ttk.Label(win, text="화씨")
+    label1.pack()
+
+    global tmp1, entry_f
+    tmp1 = tk.DoubleVar()
+    entry_f = ttk.Entry(win, textvariable=tmp1)
+    entry_f.pack()
+
+    label2=ttk.Label(win, text="섭씨")
+    label2.pack()
+
+    global tmp2, entry_c
+    tmp2 = tk.DoubleVar()
+    entry_c = ttk.Entry(win, textvariable=tmp2)
+    entry_c.pack()
+
+    btn1 = ttk.Button(win, text="화씨->섭씨", command=f_to_c)
+    btn2 = ttk.Button(win, text="섭씨->화씨", command=c_to_f)
+    btn3 = ttk.Button(win, text="초기화", command=reset)
+    btn4 = ttk.Button(win, text="종료", command=win.destroy)
+    btn1.pack()
+    btn2.pack()
+    btn3.pack()
+    btn4.pack()
+    
+def f_to_c():
+    f = tmp1.get()
+    c = (float(f) - 32) / 1.8
+    entry_c.delete(0, tk.END)
+    entry_c.insert(0, f"{c:.1f}")
+
+def c_to_f():
+    c = tmp2.get()
+    f = float(c) * 1.8 + 32
+    entry_f.delete(0, tk.END)
+    entry_f.insert(0, f"{f:.1f}")
+
+def reset():
+    entry_f.delete(0, tk.END)
+    entry_c.delete(0, tk.END)
+
+win = tk.Tk()
+win.title("온도변환기-1단계")
+buildgui()
+win.mainloop()

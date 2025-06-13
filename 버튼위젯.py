98507650634559c2a621bf6ca2365f508ee29ce2
황병규 <128976098+banggi0323@users.noreply.github.com>
2025-06-13 16:31:49 +0900

    Add files via upload

---
+++ 버튼위젯.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk
+
+def buildgui():
+    global text_label
+    text_label = ttk.Label(win, text='안녕하세요')
+
+    input_btn = ttk.Button(win, text='입력',
+                           command=input_btn_handler)
+    quit_btn = ttk.Button(win, text='종료',
+                           command=win.destroy)
+    text_label.pack()
+    input_btn.pack()
+    quit_btn.pack()
+
+def input_btn_handler():
+    text_label.configure(text='반가워요')
+    
+win = tk.Tk()
+win.title('라벨 위젯 예1')
+buildgui()
+win.mainloop()
